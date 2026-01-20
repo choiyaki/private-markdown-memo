5928627bd1da17e589b00843d4d850600e0faf83
吉田篤史 <choiyaki@gmail.com>
2026-01-20 22:22:51 +0900

    無理かもなぁ。

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-import { EditorState } from "https://unpkg.com/@codemirror/state@6.4.1/dist/index.js";
-import { EditorView } from "https://unpkg.com/@codemirror/view@6.24.1/dist/index.js";
+const editor = CodeMirror.fromTextArea(
+  document.getElementById("editor"),
+  {
+    mode: "markdown",
+    lineWrapping: true,
+    lineNumbers: false,
+    tabSize: 2,
+    indentUnit: 2,
+    extraKeys: {
+      Tab(cm) {
+        cm.execCommand("insertSoftTab");
+      },
+      "Shift-Tab"(cm) {
+        cm.execCommand("indentLess");
+      }
+    }
+  }
+);
 
 /* 初期テキスト */
-const state = EditorState.create({
-  doc: "テスト\n改行できます\n横に伸びません",
-  extensions: [
-    EditorView.lineWrapping
-  ]
-});
-
-/* エディタ生成 */
-new EditorView({
-  state,
-  parent: document.getElementById("editor")
-});
+editor.setValue(`- ああああああああああああ
+- いいいいいいいいいいいい
+  - インデントされた行が長くなっても自然に揃います
+`);
