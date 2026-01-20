b28808201868f9b24e4e2aad88c1cb0cf723ed20
吉田篤史 <choiyaki@gmail.com>
2026-01-20 22:17:49 +0900

    codemirrorの一歩目

--- app.js
+++ app.js
@@ -1,25 +1,11 @@
 import { EditorState } from "https://unpkg.com/@codemirror/state@6.4.1/dist/index.js";
-import { EditorView, keymap } from "https://unpkg.com/@codemirror/view@6.24.1/dist/index.js";
-import { defaultKeymap, indentWithTab } from "https://unpkg.com/@codemirror/commands@6.3.3/dist/index.js";
-import { markdown } from "https://unpkg.com/@codemirror/lang-markdown@6.2.5/dist/index.js";
+import { EditorView } from "https://unpkg.com/@codemirror/view@6.24.1/dist/index.js";
 
 /* 初期テキスト */
-const startDoc = `- ああああああああああああ
-- いいいいいいいいいいいい
-\t- インデントされた行が長くなっても
-\t  自然に揃います
-`;
-
-/* エディタ状態 */
 const state = EditorState.create({
-  doc: startDoc,
+  doc: "テスト\n改行できます\n横に伸びません",
   extensions: [
-    markdown(),
-    EditorView.lineWrapping, // ← ★ これを追加
-    keymap.of([
-      indentWithTab,
-      ...defaultKeymap
-    ])
+    EditorView.lineWrapping
   ]
 });
 
